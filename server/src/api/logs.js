2036f3800d978cb933583ee6ab8c71b08f388c1f
bslzz <bishalkoid@gmail.com>
2020-05-26 13:39:58 +0300

    installed react-map and other dependencies and connected frontend with backend

---
+++ server/src/api/logs.js
@@ -0,0 +1,46 @@
+const { Router } = require('express');
+const RateLimit = require('express-rate-limit');
+const MongoStore = require('rate-limit-mongo');
+const LogEntry = require('../models/LogEntry');
+
+const { API_KEY, DATABASE_URL } = process.env;
+
+const router = Router();
+
+const rateLimitDelay = 10 * 1000; // 10 second delay
+const limiter = new RateLimit({
+  store: new MongoStore({
+    uri: DATABASE_URL,
+    expireTimeMs: rateLimitDelay,
+  }),
+  max: 1,
+  windowMs: rateLimitDelay,
+});
+
+router.get('/', async (req, res, next) => {
+  try {
+    const entries = await LogEntry.find();
+    res.json(entries);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', limiter, async (req, res, next) => {
+  try {
+    if (req.get('X-API-KEY') !== API_KEY) {
+      res.status(401);
+      throw new Error('UnAuthorized');
+    }
+    const logEntry = new LogEntry(req.body);
+    const createdEntry = await logEntry.save();
+    res.json(createdEntry);
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      res.status(422);
+    }
+    next(error);
+  }
+});
+
+module.exports = router;
---
+++ traveldiariesapp/src/API.js
@@ -0,0 +1,37 @@
+const API_URL =
+  window.location.hostname === 'localhost'
+    ? 'http://localhost:5000'
+    : 'https://travel-log-api.now.sh';
+
+export async function listLogEntries() {
+  const response = await fetch(`${API_URL}/api/logs`);
+  return response.json();
+}
+
+export async function createLogEntry(entry) {
+  const apiKey = entry.apiKey;
+  delete entry.apiKey;
+  const response = await fetch(`${API_URL}/api/logs`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'X-API-KEY': apiKey,
+    },
+    body: JSON.stringify(entry),
+  });
+  let json;
+  if (response.headers.get('content-type').includes('text/html')) {
+    const message = await response.text();
+    json = {
+      message,
+    };
+  } else {
+    json = await response.json();
+  }
+  if (response.ok) {
+    return json;
+  }
+  const error = new Error(json.message);
+  error.response = json;
+  throw error;
+}
