82afa0c9645c4cb133ae992fec50df683fe97d4f
bslzz <bishalkoid@gmail.com>
2020-05-26 12:51:17 +0300

    initial commit

---
+++ traveldiariesapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
+import NavHeader from './Components/Navbar/Navbar';
+
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
---
+++ traveldiariesapp/src/Components/Navbar/Navbar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+
+const NavHeader = () => {
+  return <div></div>;
+};
+
+export default NavHeader;
