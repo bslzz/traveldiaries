f7b19ac1eeb94b3f1a29ad79f28f90382dd110a4
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2020-05-27 20:54:01 +0300

    nav bar added

--- myfrontend/src/App.js
+++ myfrontend/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import MyMap from './Components/Map/MyMap';
+import Login from './Components/Login/Login';
+import Home from './Components/Home/Home';
+import Register from './Components/Register/Register';
+import NavHeader from './Components/Navbar/Navbar';
+
 require('dotenv').config();
 
 const App = () => {
   return (
+    <div className="page-container">
+    <div className="content-wrap">
     <Router>
+      <NavHeader />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
       <MyMap />
-    </Router>
+      </Router>
+    </div>
+    </div>
   );
 };
 
--- myfrontend/src/Components/Navbar/Navbar.js
+++ myfrontend/src/Components/Navbar/Navbar.js
@@ -1,8 +1,30 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { Link} from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav } from "react-bootstrap";
 
 const NavHeader = () => {
-  return <div></div>;
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="primary" variant="primary">
+      <Navbar.Brand href="/">
+       <img id="brand-image" alt="Website logo" src="joelogo.jpg" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="ml-auto">
+          <Link className="d-inline p-2 text-white" to="/">
+            Home
+          </Link>
+          <Link className="d-inline p-2 text-white" to="/login">
+            Login
+          </Link>
+          <Link className="d-inline p-2 text-white" to="/register">
+            Register
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default NavHeader;
