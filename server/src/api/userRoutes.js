d1c45b6cbee97b9e04d0b5926049d50c8c4ba7ad
bslzz <bishalkoid@gmail.com>
2020-05-30 22:28:18 +0300

    login route created in backend

--- server/src/api/userRoutes.js
+++ server/src/api/userRoutes.js
@@ -3,16 +3,15 @@ const router = express.Router();
 const brcrypt = require('bcryptjs');
 const User = require('../models/UserSchema');
 
-router.post('/register', async (req, res) => {
+router.post('/register', (req, res) => {
   const { name, email, password } = req.body;
 
-  if (!email || !password) {
+  if (!name || !email || !password) {
     res.status(401).json({ msg: 'Email and Password required' });
   }
-
-  await User.findOne({ email }).then((savedUser) => {
+  User.findOne({ email }).then((savedUser) => {
     if (savedUser) {
-      res.status(422).json('User alreade exists');
+      return res.status(422).json('User already exists');
     }
 
     brcrypt.hash(password, 10).then((hashedPassword) => {
@@ -29,9 +28,35 @@ router.post('/register', async (req, res) => {
           console.log(req.body);
         })
         .catch((err) => {
-          res.status(422).json({ msg: 'Error saving new user to DB' });
+          console.log({ msg: 'Error saving new user to DB' });
         });
     });
+  });
+});
+
+router.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(401).json({ msg: 'Email and Password required' });
+  }
+
+  User.findOne({ email }).then((savedUser) => {
+    if (!savedUser) {
+      res.status(422).json({ msg: 'No user found' });
+    }
+    brcrypt
+      .compare(password, savedUser.password)
+      .then((matched) => {
+        if (matched) {
+          res.status(200).json('Matched');
+        } else {
+          res.status(422).json('Invalid email or password');
+        }
+      })
+      .catch((err) => {
+        console.log(`Error logging in: ${err}`);
+      });
   });
 });
 
